2d36262ece4d5ce1a20319471578957d5fa8d288
Simon Universal <simonuniversal5@gmail.com>
2026-02-19 13:38:28 +0200

    create_rsvps_table check if column already exisits

--- database/migrations/2026_02_12_082120_create_rsvps_table.php
+++ database/migrations/2026_02_12_082120_create_rsvps_table.php
@@ -9,25 +9,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('rsvps', function (Blueprint $table) {
-            $table->string('name')->after('id');
-            $table->string('email')->unique()->after('name');
-            $table->integer('guests')->default(1)->after('email');
-            $table->string('status')->default('pending')->after('guests');
-            $table->timestamps();
+            if (!Schema::hasColumn('rsvps', 'name')) {
+                $table->string('name')->after('id');
+            }
+            if (!Schema::hasColumn('rsvps', 'email')) {
+                $table->string('email')->unique()->after('name');
+            }
+            if (!Schema::hasColumn('rsvps', 'guests')) {
+                $table->integer('guests')->default(1)->after('email');
+            }
+            if (!Schema::hasColumn('rsvps', 'status')) {
+                $table->string('status')->default('pending')->after('guests');
+            }
+            if (!Schema::hasColumn('rsvps', 'created_at')) {
+                $table->timestamps();
+            }
         });
     }
 
     public function down(): void
     {
         Schema::table('rsvps', function (Blueprint $table) {
-            $table->dropColumn([
-                'name',
-                'email',
-                'guests',
-                'status',
-                'created_at',
-                'updated_at'
-            ]);
+            $table->dropColumn(['name', 'email', 'guests', 'status', 'created_at', 'updated_at']);
         });
     }
 };
